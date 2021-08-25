ae00eea641ff16e865467657841afa6c230d34f6
qkrthdus605 <qkrthdus605@gmail.com>
2021-08-25 22:44:39 +0900

    first commit : Fin Star

---
+++ CodingTestStudy/src/com/company/Star.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class Star {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=0;
+        int m=0;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int j=0; j< m; j++){
+            for(int i=0; i < n; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
