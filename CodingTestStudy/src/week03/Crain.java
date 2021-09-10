50f5435784df482a542a6e65a4159043ee5a4ff5
qkrthdus605 <qkrthdus605@gmail.com>
2021-09-10 22:50:36 +0900

    체육복 문제 해결 : lost와 reserve의 정렬이 필요

---
+++ CodingTestStudy/src/week03/Crain.java
@@ -0,0 +1,12 @@
+package week03;
+// 크레인 인형뽑기 문제
+// 2019 카카오 개발자 겨울 인턴십
+// 스택을 사용하자!
+// [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]	[1,5,3,5,1,2,1,4]	4
+public class Crain {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        return answer;
+    }
+}
--- CodingTestStudy/src/week03/Physical.java
+++ CodingTestStudy/src/week03/Physical.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 public class Physical {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
         // n = 5 lost= 1 4 5 reserve = 2 5
-        // lost 1 3 0 reserve 2 0
+        // lost 1 3 -1 reserve 2 -1
 
         // 잃어버린 사람과 여분의 체육복이 있는 사람이 같을 경우 빌려줄 수 없음
         // 하지만 체육복을 가지고 있으므로 answer++
-        // 이에 해당하는 학생을 0으로 처리
+        // 이에 해당하는 학생을 -1로 처리
         for(int i = 0; i < lost.length; i++){
             for(int j = 0; j < reserve.length; j++){
                 if(lost[i] == reserve[j]){
-                    lost[i] = 0;
-                    reserve[j] = 0;
+                    lost[i] = -1;
+                    reserve[j] = -1;
                     answer++;
                     break;
                 }
@@ -26,12 +28,12 @@ public class Physical {
         for(int i = 0; i < lost.length; i++){
             for(int j = 0; j < reserve.length; j++){
                 // 둘 중 하나가 0이면 빌려주거나 빌릴 수 없음
-                if(lost[i]== 0 || reserve[j] == 0){
+                if(lost[i]== -1 || reserve[j] == -1){
                     continue;
                 }
                 // 이웃한 학생끼리만 빌려줄 수 있다
                 // 두 학생의 차의 절대값이 1이면 체육복을 빌려줄 수 있음
-                // 빌려준 학생과 빌린 학생은 0으로 처리
+                // 빌려준 학생과 빌린 학생은 -1로 처리
                 if(Math.abs(lost[i]-reserve[j]) == 1){
                     answer++;
                     reserve[j] = 0;
