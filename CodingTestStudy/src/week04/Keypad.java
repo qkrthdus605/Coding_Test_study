f2b3098b03b9d8c23e0bee0130b84e5cdd4906a2
qkrthdus605 <qkrthdus605@gmail.com>
2021-09-16 23:08:14 +0900

    '키패드 누르기' 문제 해결

--- CodingTestStudy/src/week04/Keypad.java
+++ CodingTestStudy/src/week04/Keypad.java
@@ -15,6 +15,11 @@ public class Keypad {
 
         for(int i = 0; i < numbers.length; i++){
 
+            if(numbers[i]==0){
+                // 거리 계산을 위해 0을 11로 치환
+                numbers[i] = 11;
+            }
+
             if(numbers[i]%3 == 1){
                 // 1, 4, 7 (3으로 나눠 나머지가 1인 숫자)라면 왼손으로 누른다
                 answer = answer + "L";
@@ -27,30 +32,31 @@ public class Keypad {
 
             }else{
                 // 2, 5, 8, 0 : 거리를 계산한 후 가까운 손으로 누른다
-                // 왼손, 오른손과의 거리 구하기
-                int left_dis = 0;
-                int right_dis = 0;
-                // 거리를 어떻게 구해야하즤.......
 
-                if(left_dis > right_dis){  // 왼손이 더 멀면 오른손으로 누른다
-                    answer = answer + "R";
-                    right = numbers[i];
-                }else if(left_dis < right_dis){   // 오른손이 더 멀면 오른손으로 누른다
+                // 왼손, 오른손과의 거리 구하기
+                int left_dis = Math.abs((left-numbers[i]))/3 + Math.abs((left-numbers[i]))%3;
+                int right_dis = Math.abs((right-numbers[i]))/3 + Math.abs((right-numbers[i]))%3;
+
+                if(right_dis > left_dis){
                     answer = answer + "L";
                     left = numbers[i];
-                }else{   // 거리가 같으면 해당 hand로 누른다
-                    if(hand == "Right"){
-                        answer = answer + "R";
-                        right = numbers[i];
-                    }else{
+                }else if(right_dis < left_dis){
+                    answer = answer + "R";
+                    right = numbers[i];
+                }else{
+                    if(hand.equals("left")){
                         answer = answer + "L";
                         left = numbers[i];
+                    }else{
+                        answer = answer + "R";
+                        right = numbers[i];
                     }
                 }
             }
         }
         return answer;
     }
+
     public static void main(String[] args){
 
         int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
